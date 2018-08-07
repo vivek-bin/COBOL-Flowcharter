b85f5cd92b69f8fd15404eb26d2e1136cd5606bc
BINDAL Vivek <vivek.bindal@soprasteria.com>
2018-08-07 17:15:07 +0530

    flowcharter in progress

--- src/flow.py
+++ src/flow.py
@@ -1,33 +1,67 @@
 from Tkinter import Tk, Canvas, Frame, Button
 from Tkinter import BOTH, W, NW, SUNKEN, TOP, X, FLAT, LEFT
+from PIL import Image, ImageTk
+import tooltip
+
+class ButtonProperties:
+	def __init__(self):
+		self.button = False
+		self.buttonWindow = False
+		self.buttonTooltip = False
 
 class Example(Frame):
 	def __init__(self, parent):
 		Frame.__init__(self, parent)
 		self.parent = parent
+		self.lines=[]
 		self.initUI()
 
+	def addButton(self,text,x,y,command,hoverMsg=False):
+		newButton = ButtonProperties()
+		
+		newButton.button = Button(self.canvas, text = text, command = command,anchor = W)
+		newButton.button.configure(image=self.img, compound="bottom", width = 90, activebackground = "#33B5E5",relief = FLAT)
+		newButton.buttonWindow = self.canvas.create_window(x, y, anchor=NW, window=newButton.button)
+		if hoverMsg:
+			newButton.buttonTooltip = tooltip.CreateToolTip(newButton.button, hoverMsg)
+		
+		return newButton
+		
+		
+	def createLine(self,x1,y1,x2,y2,fill="red",width=5):
+		def f1(e):
+			self.canvas.itemconfigure(self.canvas.find_withtag("current")[0],width=9)
+		def f2(e):
+			self.canvas.itemconfigure(self.canvas.find_withtag("current")[0],width=5)
+		line = self.canvas.create_line(x1, y1, x2, y2, fill=fill,width=width)
+		#self.canvas.tag_bind(line, '<ButtonPress-1>', f1)
+		print self.canvas.tag_bind(line, '<Enter>', f1)
+		print self.canvas.tag_bind(line, '<Leave>', f2)
+		#self.lines.append(line)
+		#tooltip.CreateToolTip(line, "sgvgwegrwergwre")
+	
 	def initUI(self):
 		self.parent.title("Layout Test")
 		self.config(bg = '#F0F0F0')
 		self.pack(fill = BOTH, expand = 1)
 		#create canvas
-		canvas1 = Canvas(self, relief = FLAT, background = "#D2D2D2",width = 180, height = 500)
-		canvas1.pack(side = TOP, anchor = NW, padx = 10, pady = 10)
-		canvas1.create_line(0, 0, 200, 100, fill="red",width=10)
+		self.canvas = Canvas(self, relief = FLAT, background = "#D2D2D2",width = 180, height = 500)
+		self.canvas.pack(side = TOP, anchor = NW, padx = 10, pady = 10)
+		
+		self.img = ImageTk.PhotoImage(Image.open("D:\\Profiles\\vbindal\\Downloads\\io-flow.png"))
+		
 		#add quit button
-		button1 = Button(canvas1, text = "Quit", command = self.quit,anchor = W)
-		button1.configure(width = 10, activebackground = "#33B5E5",relief = FLAT)
-		button1_window = canvas1.create_window(10, 10, anchor=NW, window=button1)
-		#button1.pack(side = TOP)
-		#add quit button
-		button2 = Button(canvas1, text = "Quit", command = self.quit,anchor = W)
-		button2.configure(activebackground = "#33B5E5")
-		button2_window = canvas1.create_window(10, 90, anchor=NW, window=button2)
-		#button2.pack(side = TOP)
-
+		
+		self.createLine(0,100,100,0)
+		self.createLine(0,0,200,200)
+		#self.addButton("quit",10,10,self.quit,"rhnrhsreth")
+		self.addButton("quit",10,90,self.quit)
+		
 def main():
 	root = Tk()
 	root.geometry('800x600+10+50')
 	app = Example(root)
 	app.mainloop()
+	root.destroy()
+
+main()
--- src/testing.py
+++ src/testing.py
@@ -20,21 +20,40 @@ def t9(component="VIID246"):
 #os.system('"C:\Program Files\Notepad++\notepad++.exe" "d:\Profiles\vbindal\Desktop\2389 email jcl.txt" -n65')
 
 
-def t1():
-    root = tk.Tk()
-    btn1 = tk.Button(root, text="button 1")
-    btn1.pack(padx=10, pady=5)
-    button1_ttp = tooltip.CreateToolTip(btn1, \
-   'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, '
-   'consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum '
-   'quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam '
-   'est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.')
+from Tkinter import *
 
-    btn2 = tk.Button(root, text="button 2")
-    btn2.pack(padx=10, pady=5)
-    button2_ttp = CreateToolTip(btn2, \
-    "First thing's first, I'm the realest. Drop this and let the whole world "
-    "feel it. And I'm still in the Murda Bizness. I could hold you down, like "
-    "I'm givin' lessons in  physics. You should want a bad Vic like this.")
-    root.mainloop()
-	
+root = Tk()
+
+canvas = Canvas(root, width=400, height=200)
+canvas.pack()
+canvas.create_oval(10, 10, 110, 60, fill="grey")
+canvas.create_text(60, 35, text="Oval")
+canvas.create_rectangle(10, 100, 110, 150, outline="blue")
+canvas.create_text(60, 125, text="Rectangle")
+canvas.create_line(60, 60, 60, 100, width=3)
+
+class MouseMover():
+    def __init__(self):
+        self.item = 0; self.previous = (0, 0)
+    def select(self, event):
+        widget = event.widget           # Get handle to canvas
+        # Convert screen coordinates to canvas coordinates
+        xc = widget.canvasx(event.x); yc = widget.canvasx(event.y)
+        self.item = widget.find_closest(xc, yc)[0]      # ID for closest
+        self.previous = (xc, yc)
+        print((xc, yc, self.item))
+    def drag(self, event):
+        widget = event.widget
+        xc = widget.canvasx(event.x); yc = widget.canvasx(event.y)
+        canvas.move(self.item, xc-self.previous[0], yc-self.previous[1])
+        self.previous = (xc, yc)
+
+
+# Get an instance of the MouseMover object
+mm = MouseMover()
+
+# Bind mouse events to methods (could also be in the constructor)
+canvas.bind("<Button-1>", mm.select)
+canvas.bind("<B1-Motion>", mm.drag)
+
+root.mainloop()
